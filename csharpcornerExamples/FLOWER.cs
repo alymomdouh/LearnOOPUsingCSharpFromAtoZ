058f113876c6c2dfe6202e7179ea7a6bb767ef9b
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2022-11-20 14:36:22 +0200

    Add project files.

---
+++ csharpcornerExamples/FLOWER.cs
@@ -0,0 +1,15 @@
+﻿namespace csharpcornerExamples
+{
+    public class FLOWER
+    {
+        public string flowercolor;
+        public FLOWER(string color)
+        {
+            this.flowercolor = color;
+        }
+        public string display()
+        {
+            return "Color of the flower : " + this.flowercolor;
+        }
+    }
+}
---
+++ csharpcornerExamples/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharpcornerExamples;
+
+Console.WriteLine("Hello, World!");
+FLOWER flow;
+flow = new FLOWER("YELLOW");
+Console.WriteLine(flow.display());
+Console.ReadLine();
